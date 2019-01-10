29774bf556a383db34490a05f29f265f7ca12352
smilecana <smilecana@gmail.com>
2019-01-10 12:16:09 -0400

    In Class Exercise

---
+++ Labs/Lab2/src/ca/prog1400/Main.java
@@ -0,0 +1,9 @@
+package ca.prog1400;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Purchase user1 = new Purchase();
+        user1.calculateShipping();
+    }
+}
---
+++ Labs/Lab2/src/ca/prog1400/Purchase.java
@@ -0,0 +1,21 @@
+package ca.prog1400;
+import java.util.Scanner;
+
+public class Purchase {
+    public void calculateShipping(){
+        double cost;
+        double total;
+        System.out.println("welcome Shopper:) ");
+        System.out.println("==================");
+        System.out.println("Enter amount for their total purchase?");
+        Scanner scanner = new Scanner(System.in);
+        cost = scanner.nextInt() ;
+        if(cost < 50 ){
+            total = cost + 10;
+            System.out.println("Your total is " + total);
+        }else{
+            total = cost + 0;
+            System.out.println("Your total is " + total + "and free shipping!!");
+        }
+    }
+}
