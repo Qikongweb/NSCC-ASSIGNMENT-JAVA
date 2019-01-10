cac6b6961e0ab1c7e7e356e77c9a824cbaebe891
smilecana <smilecana@gmail.com>
2019-01-09 21:05:01 -0400

    git Test

--- Demo/Lab1/src/ca/prog1400/MakingCookies.java
+++ Demo/Lab1/src/ca/prog1400/MakingCookies.java
@@ -3,14 +3,18 @@ package ca.prog1400;
 public class MakingCookies {
     int defaultCounter = 1;
     String defaultTasty = "chocolate";
+    int defaultPrice = 20;
 
     public void setCounter(int customerCounter){
         this.order  = customerCounter;
     }
+
     public void setPrice(double newPrice){
         this.price = newPrice;
     }
+
     public void setTasty(String customerTasty){
         this.tasty = customerTasty;
     }
+
 }
