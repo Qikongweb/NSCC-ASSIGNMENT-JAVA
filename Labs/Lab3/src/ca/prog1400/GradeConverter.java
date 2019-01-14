b88fa149cb669ed0834eed3a03c92db90b9af534
smilecana <smilecana@gmail.com>
2019-01-14 10:23:40 -0400

    I changed String to char

--- Labs/Lab3/src/ca/prog1400/GradeConverter.java
+++ Labs/Lab3/src/ca/prog1400/GradeConverter.java
@@ -2,19 +2,21 @@ package ca.prog1400;
 import java.util.Scanner;
 
 public class GradeConverter {
-    private static String[] gradeLetter = {"A", "B", "C", "D", "F"};
-    private static double[] gradeNumber = {4,3,2,1,0};
+//    private static String[] gradeLetter = {"A", "B", "C", "D", "F"};
+    private static char[] gradeLetter = {'A', 'B', 'C', 'D', 'F'};
+    private static double[] gradeNumber = {4.0,3.0,2.0,1.0,0.0};
 
     public static void main(String[] args) {
         System.out.println("Welcome :)");
         System.out.println("What is your grade?? ");
         Scanner scanner = new Scanner(System.in);
-        String myPoint = String.format("Your grade is %.2f", GradeConverter.converter(scanner.next()));
+        char letter = scanner.next().charAt(0);
+        String myPoint = String.format("Your grade is %s and point is %.2f",letter, GradeConverter.converter(letter));
         System.out.println(myPoint);
     }
-    private static double converter(String userLetter) {
+    private static double converter(char userLetter) {
         for (int i = 0; i < gradeLetter.length; i++) {
-            if (userLetter.equals(gradeLetter[i])) {
+            if (userLetter==gradeLetter[i]) {
                 return gradeNumber[i];
             }
         }
