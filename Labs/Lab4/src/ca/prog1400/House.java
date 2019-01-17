b2a28782bde0b1aae30a1302d50334c8eb494b31
smilecana <smilecana@gmail.com>
2019-01-17 13:29:00 -0400

    In class  acitivy :)

--- Labs/Lab4/src/ca/prog1400/House.java
+++ Labs/Lab4/src/ca/prog1400/House.java
@@ -1,5 +1,29 @@
 package ca.prog1400;
 
-public class House {
+import java.awt.*;
+import java.util.Scanner;
 
+public class House {
+    double length;
+    double weight;
+    String roofColour = "Black";
+    boolean changeRoofColour;
+
+    public House(double l, double w, boolean changeRoofColour){
+        this.length = l;
+        this.weight = w;
+        this.changeRoofColour = changeRoofColour;
+        if(changeRoofColour){
+            this.roofColour = chooseRoofColor();
+        }
+
+    }
+    public void printTotalArea(){
+        System.out.println(String.format("the house size is %.2f & roof color is %s", length * weight , roofColour));
+    }
+    private String chooseRoofColor(){
+        System.out.println("You can choose color you want. what do you want color?");
+        Scanner sc = new Scanner(System.in);
+        return sc.next();
+    }
 }
--- Labs/Lab4/src/ca/prog1400/Main.java
+++ Labs/Lab4/src/ca/prog1400/Main.java
@@ -1,9 +1,14 @@
 package ca.prog1400;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+    House myHouse1 = new House(30,40,true);
+    House myHouse2 = new House(40,40, false);
+    myHouse1.printTotalArea();
+    myHouse2.printTotalArea();
 
     }
 }
