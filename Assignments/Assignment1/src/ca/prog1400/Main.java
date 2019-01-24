9304cc17fbdeaa295364f0c906834fe9922ee810
smilecana <smilecana@gmail.com>
2019-01-24 12:22:09 -0400

    Added validate

--- Assignments/Assignment1/src/ca/prog1400/Main.java
+++ Assignments/Assignment1/src/ca/prog1400/Main.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("FANTASY HOCKEY APPLICATION");
         System.out.println("TEAM ENTRY");
@@ -17,7 +16,7 @@ public class Main {
         for (int i = 0; i <teams.length ; i++) {
             System.out.println("Enter name for team # " + (i + 1) + ":");
             teams[i] = new Team();
-            teams[i].setTeamName(scanner.next());
+            teams[i].setTeamName(checkName());
         }
 
         for(Team team : teams){
@@ -26,11 +25,11 @@ public class Main {
             team.setPlayer(new Player[1]);
             for(int j =0 ; j<team.getPlayer().length;j++){
                 System.out.println("Enter name for player #" + team.getTeamName() + ":");
-                team.getPlayer()[j] = new Player(scanner.next());
+                team.getPlayer()[j] = new Player(checkName());
                 System.out.println("Enter number of goals for " + team.getPlayer()[j].getName());
-                team.getPlayer()[j].setGolas(scanner.nextInt());
+                team.getPlayer()[j].setGolas(checkNumber());
                 System.out.println("Enter number of assists for " + team.getPlayer()[j].getName());
-                team.getPlayer()[j].setAssists(scanner.nextInt());
+                team.getPlayer()[j].setAssists(checkNumber());
             }
             team.setBudget(new Random().nextFloat()*100000);
 
@@ -47,6 +46,37 @@ public class Main {
         for(Team team:teams){
             team.outputPlayerDetails();
         }
+
+    }
+    private static String checkName(){
+        String result = "";
+        Scanner scanner = new Scanner(System.in);
+        do{
+           while(!scanner.hasNextLine()){
+               if(scanner.nextLine().length() < 3){
+                   System.out.println("Please enter team name  least 3 characters.");
+               }
+               System.out.println("Please enter a valid word!");
+               scanner.next();
+           }
+           result = scanner.nextLine();
+        }while(result == null || result.equals("") || result.length() < 3);
+
+        return result;
+    }
+
+    private static int checkNumber(){
+        int result = 0;
+        Scanner scanner = new Scanner(System.in);
+        do{
+            while(!scanner.hasNextInt()){
+                System.out.println("Please enter a number  greater than 0.");
+                scanner.next();
+            }
+            result = scanner.nextInt();
+        }while(result < 0);
+
+        return result;
     }
 }
 
